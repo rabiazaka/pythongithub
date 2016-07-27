243786e5ff0238270e30e37f1aa679d4e7ad6e74
rabiazaka <rsiddiqui011@gmail.com>
2016-07-27 19:07:07 +0500

    chapter 2
    chapter 2

---
+++ program 1 data type.py
@@ -0,0 +1,27 @@
+message= "python is my favourite language" \
+         "hello world"
+print(message)
+
+name='rabia siddiqqui'
+print(name.upper())
+print(name.title())
+
+
+firstname='rabia'
+lastname='siddiqui'
+fullname=firstname +" "+ lastname
+message='hello ' +""+ fullname +" "+"!"
+print(message)
+message='hello ' +""+ fullname.upper() +" "+"!"
+print(message)
+message='hello ' +""+ fullname.lower() +" "+"!"
+print(message)
+message='hello ' +""+ fullname.title() +" "+"!"
+print(message)
+
+famousperson='Albert einstein'
+message="A person who never made amistake never tried anything new!"
+print(famousperson +" " +"once said" +" "+ "'"+message+"'")
+
+age=23
+print("Happy" +" "+ str(age)+""+ "rd Birthday!")
